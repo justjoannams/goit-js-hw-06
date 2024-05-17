ade6e1b5145fac042d4781e651c6c38aecddc080
justjoannams <justjoannams@gmail.com>
2024-05-17 15:29:50 +0800

    goit-js-hw-06-task-09

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const color = document.querySelector("span.color");
+const colorChangerButton = document.querySelector("button.change-color");
+
+colorChangerButton.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
